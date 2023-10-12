f0fc639cb1069468c9a7d6e9e43603ae81305f56
Migue27au <43569365+migue27au@users.noreply.github.com>
2023-10-12 18:59:13 +0200

    Add files via upload

--- auto_kali_configuration.sh
+++ auto_kali_configuration.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+
+function log {
+    text=$1
+    BOLDRED="\e[1;31m" # Rojo en negrita
+    END="\e[0m" # Resetear los estilos y colores
+
+    echo -e "${BOLDRED}${text}${END}"
+}
+
 whoami=$(whoami)
 
-if [[ $whoami -ne "root" ]]; then
-	echo "Run this with sudo"
+if [[ $whoami != "root" ]]; then
+	log "Run this with sudo"
 	exit 0
 fi
 
-echo ""
+log "Detected user: $whoami"
 
-echo "Detected user: $whoami"
-
-echo "Downloading wallpaper..."
+log "Downloading wallpaper..."
 
 cd /home/$whoami/Pictures
 wget https://raw.githubusercontent.com/migue27au/auto_kali_configuration/main/wallpaper.png
 
-echo "Setting wallpaper..."
+log "Setting wallpaper..."
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVirtual1/workspace0/last-image -s /home/$whoami/Pictures/wallpaper.png
 
-echo "Changing keyboard layout to es"
+log "Changing keyboard layout to es"
 setxkbmap es
