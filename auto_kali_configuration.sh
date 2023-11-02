f425edc5dc73d2a72d3f350cd7a48996745977b2
Migue27au <43569365+migue27au@users.noreply.github.com>
2023-11-02 10:49:30 +0100

    Update auto_kali_configuration.sh

--- auto_kali_configuration.sh
+++ auto_kali_configuration.sh
@@ -38,7 +38,7 @@ echo "deb [arch=amd64] https://download.docker.com/linux/debian bullseye stable"
 
 apt update
 
-apt install -y dbus-x11 sshpass google-chrome-stable sublime-text dirmngr gnupg xfce4-terminal snapd tldr flameshot bloodhound keepass2 brave-browser golang xfce4-genmon-plugin gimp vlc audacity bat docker-ce docker-ce-cli containerd.io bettercap hostapd mdk4 asleap isc-dhcp-server hostapd-wpe hcxdumptool hcxtools beef-xss lighttpd
+apt install -y dbus-x11 sshpass google-chrome-stable sublime-text dirmngr gnupg xfce4-terminal snapd tldr flameshot bloodhound keepass2 brave-browser golang xfce4-genmon-plugin gimp vlc audacity bat docker-ce docker-ce-cli containerd.io bettercap hostapd mdk4 asleap isc-dhcp-server hostapd-wpe hcxdumptool hcxtools beef-xss lighttpd openvpn-systemd-resolved
 
 
 user=$(ls /home)
